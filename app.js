68ae938220b3e4627a7699b87d2bebf27140338d
Beau Carnes <beaucarnes@gmail.com>
2018-02-06 14:05:50 -0500

    removing API key

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = `81f316c5f31960d155555818b8d0a59c` // Fill this in with your own API key from https://scripture.api.bible/
+const API_KEY = `` // Fill this in with your own API key from https://scripture.api.bible/
 
 /**
  * Fills in list on page with Bible versions.
